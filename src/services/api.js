0e65e7917ddecf95ec1c6de9a68983399946f667
Suraj Kembale <surajkembale542@gmail.com>
2026-03-26 23:01:11 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import { useAuthStore } from '../context/store.js'; // Import your store
 const API_BASE_URL = 'https://ai-powered-research-paper-summarizer.onrender.com/'|| import.meta.env.VITE_API_URL ||'http://127.0.0.1:8000'||'https://ai-powered-research-paper-summarizer.onrender.com/';
 
 const apiClient = axios.create({
-  baseURL: `${API_BASE_URL}/api`,
+  baseURL: `${API_BASE_URL.replace(/\/$/, "")}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
